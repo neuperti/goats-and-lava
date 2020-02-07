43a959469b922dd250b7e9bcc00a356993ea3a8b
Elemen <dienste@neuperti.de>
2020-02-07 17:01:35 +0100

    minor changes in grid.py

--- gui/grid.py
+++ gui/grid.py
@@ -4,36 +4,43 @@ from pathlib import Path
 from gui.cells import Cell
 
 
-IMAGE_WIDH = 1000
+IMAGE_WIDTH = 1000
 
 
 def absolute_path(relative_path):
     """turns a relative path (with leading '/') into an absolute one"""
     base_folder = os.path.dirname(__file__)
     base_folder = str(Path(base_folder).parent)
-    absolute_path = base_folder + relative_path
-    return absolute_path
+    absolute_path_name = base_folder + relative_path
+    return absolute_path_name
+
 
 class Grid(tk.Frame):
+    """a class which manages the visual appearence of the grid"""
     def __init__(self, master, queue, size=10):
+        """initializes the class"""
         tk.Frame.__init__(self, master)
         self.cell_function = None
         self.cells = dict()
         self.size = size
-        #os.
         self.queue = queue
         self.images = dict()
-        # Images
         self.screen_width = master.winfo_screenwidth()
         self.screen_height = master.winfo_screenheight()
         image_filenames = os.listdir(absolute_path("/images"))
         for filename in image_filenames:
             image = tk.PhotoImage(file=absolute_path("/images/" + filename))
             image = image.zoom(int(self.screen_height * .9) // 100)
-            image = image.subsample(size*IMAGE_WIDH//100)  # adapts image depending on grid size and screen size
+            image = image.subsample(size*IMAGE_WIDTH//100)  # adapts image depending on grid size and screen size
             self.images[filename] = image
         print(self.images)
         self.update_size(size)
+        self.after(1000, self.check_print_queue)
+
+    def check_print_queue:
+        v = self.queue.get(wait_for_content=False)
+        if v:
+            self.draw(*v)
 
     def update_size(self, size):
         """generates grid of cells"""
@@ -55,12 +62,10 @@ class Grid(tk.Frame):
 
     def change_function_of_cells(self, function):
         """changes the cell function when pressed"""
-        # ändert die an jede Zelle angebundene Funktion zu function
-        # platzieren (init), nichts (defensiver modus), "bb " + "".join(coordinate)
-        # in queue reihen (offensiver modus)
         self.cell_function = function
 
-    def draw(self, size,  standard_image: str, *set_image_doubles):
+    def draw(self, standard_image: str, *set_image_doubles):
+        """updates the images of cells which changed"""
         all_cell_coordinates = set()
         standard_image = self.images[standard_image]
         # set_image_doubles are
@@ -74,9 +79,6 @@ class Grid(tk.Frame):
             if 0 not in coordinate:  # assuring, that labels are not regarded as proper cells
                 self.cells[coordinate].update_image(standard_image)
 
-    def test_function(self):
-        """for testing only"""
-        print("YAY!")
 
 
 if __name__ == "__main__":
@@ -84,7 +86,6 @@ if __name__ == "__main__":
     root = tk.Tk()
     test_grid = Grid(root, None)
     test_grid.pack()
-    test_grid.draw(10, "grass.png", ("goat.png", {(2, 4), (8, 2)}), ("stone_grass.png", {(1, 1), (5, 5)}),
-                   ("lava.png", {(3, 4), (3, 5), (3, 7)}), ("stone_lava.png", {(3, 6)})
-                   )
+    test_grid.draw("grass.png", ("goat.png", {(2, 4), (8, 2)}), ("stone_grass.png", {(1, 1), (5, 5)}),
+                   ("lava.png", {(3, 4), (3, 5), (3, 7)}), ("stone_lava.png", {(3, 6)}))
     root.mainloop()
