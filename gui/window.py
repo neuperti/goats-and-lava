f7040c0cc454fef6b559b6a38b4e5e5bc9e2c871
Elemen <dienste@neuperti.de>
2020-02-08 01:37:24 +0100

    something, idk

--- gui/window.py
+++ gui/window.py
@@ -22,6 +22,9 @@ class MainWindow(tk.Tk):
         self.title("Goats & Lava")
         self.intro_button = IntroButton(self, lambda: self.after_intro(board, queue))
         self.intro_button.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.skip_intro_button = SkipIntroButton(self, lambda: self.after_intro(board, queue))
+        self.skip_intro_button.pack(fill=tk.X)
+
 
     def after_intro(self, board, queue):
         self.grid = Grid(self, queue)
@@ -31,6 +34,7 @@ class MainWindow(tk.Tk):
         self.grid.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.console_manager.pack(side=tk.LEFT, fill=tk.Y)
         self.intro_button.pack_forget()
+        self.skip_intro_button.pack_forget()
         threading.Thread(target=lambda *args: start_game(queue, board)).start()
 
     def finish_initialisation(self):
@@ -46,16 +50,16 @@ class IntroButton(tk.Button):
         self.after_intro = after_intro
         self.loading_screen = tk.PhotoImage(file=absolute_path("/intro_images/loading_screen.png"))
         self.loading_screen = image_in_good_size(self.loading_screen, self.loading_screen.height(),
-                                                 master.winfo_screenheight())
+                                                 master.winfo_screenheight()*.9)
         self.intro_images = []
         for i in range(1, 8):
             image_path = absolute_path("/intro_images/" + str(i) + ".png")
             file = tk.PhotoImage(file=image_path)
-            file = image_in_good_size(file, file.height(), master.winfo_screenheight())
+            file = image_in_good_size(file, file.height(), master.winfo_screenheight()*.9)
             self.intro_images.append(file)
         self.title_screen = tk.PhotoImage(file=absolute_path("/intro_images/title_screen.png"))
         self.title_screen = image_in_good_size(self.title_screen, self.title_screen.height(),
-                                               master.winfo_screenheight())
+                                               master.winfo_screenheight()*.9)
         tk.Button.__init__(self, master, image=self.title_screen, padx=0, pady=0, bd=0,
                            relief=tk.FLAT, highlightthickness=0, command=self.button_pressed)
 
@@ -66,6 +70,13 @@ class IntroButton(tk.Button):
         else:
             self.configure(image=self.loading_screen)
             self.after_intro()
+
+class SkipIntroButton(tk.Button):
+    def __init__(self, master, after_intro):
+        self.after_intro = after_intro
+        tk.Button.__init__(self, master, text = "I am impatient and want to skip the intro. :(", command=self.button_pressed)
+    def button_pressed(self):
+        self.after_intro()
 
 
 if __name__ == "__main__":
